570da985b69d7c37cd61c2bc9eee6966dec837d9
brayoo2429 <brianmwiti825@gmail.com>
2025-07-18 06:35:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+
+import asyncio
+import websockets
+import json
+from flask import Flask, jsonify
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_trades():
+    return asyncio.run(fetch_trades())
+
+async def fetch_trades():
+    uri = "wss://ws.deriv.com/websockets/v3"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps({
+            "authorize": "evmoePiCZblyYJL"  # Replace with your real Deriv token
+        }))
+        await websocket.recv()  # Auth response
+
+        await websocket.send(json.dumps({
+            "statement": 1,
+            "limit": 5,
+            "description": 1
+        }))
+        response = await websocket.recv()
+        return jsonify(json.loads(response))
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8000)
+
+if __name__ == '__main__':
+    Thread(target=run_flask).start()
